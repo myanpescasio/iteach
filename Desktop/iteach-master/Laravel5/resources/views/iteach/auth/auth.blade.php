d9cceddfd90ca2f96e838bbe04ec6739a523121b
YannaAlghamdi <yannaalghamdi@gmail.com>
2015-03-26 21:12:38 +0800

    View by guest with new dashboard interface

---
+++ Desktop/iteach-master/Laravel5/resources/views/iteach/auth/auth.blade.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    @if($data['type'] == 'li')
+		<title>Login</title>
+	@else
+		<title>Sign Up</title>
+	@endif	
+
+    <!-- css -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+	<link href="css/nivo-lightbox.css" rel="stylesheet" />
+	<link href="css/nivo-lightbox-theme/default/default.css" rel="stylesheet" type="text/css" />
+	<link href="css/animations.css" rel="stylesheet" />
+    <link href="css/style.css" rel="stylesheet">
+	<link href="color/default.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-custom" class="color-dark bg-gray">
+	
+	<!-- Section: SIGN UP -->
+    <section id="sign-up" class="home-section text-center">
+		<div class="container marginbot-50">
+			<div class="row">
+				<div class="col-lg-8 col-lg-offset-2">
+					<div class="animatedParent">
+					<div class="section-heading text-center">
+					<img id="sign-up-logo" src="img/logo-temp.png" class="marginbot-50">
+					
+						@if($data['type'] == 'li')
+							@if (count($data['errors']) > 0)
+								<h2 id="sign-up-title" class="h-bold">LOGIN</h2>
+							@else
+								<h2 id="sign-up-title" class="h-bold animated bounceInDown">LOGIN</h2>
+							@endif
+						@else
+							@if (count($data['errors']) > 0)
+								<h2 id="sign-up-title" class="h-bold">SIGN UP</h2>
+							@else
+								<h2 id="sign-up-title" class="h-bold animated bounceInDown">SIGN UP</h2>
+							@endif
+						@endif
+					</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!-- Sign Up Form -->
+		<div class="container">
+			@if($data['type'] == 'li')
+				<div class="row marginbot-80">
+					<div class="col-lg-8 col-md-offset-3">
+			@else
+				<div class="row">
+					<div class="col-lg-8 col-md-offset-2">
+			@endif
+					@yield('content');
+
+				</div>
+			</div>	
+		</div>
+	</section>
+	<!-- /Section: SIGN UP -->
+
+
+    <!-- Core JavaScript Files -->
+    <script src="js/jquery.min.js"></script>	 
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	<script src="js/jquery.sticky.js"></script>
+    <script src="js/jquery.easing.min.js"></script>	
+	<script src="js/jquery.scrollTo.js"></script>
+	<script src="js/jquery.appear.js"></script>
+	<script src="js/stellar.js"></script>
+	<script src="js/nivo-lightbox.min.js"></script>
+	
+    <script src="js/custom.js"></script>
+	<script src="js/css3-animate-it.js"></script>
+
+</body>
+
+</html>
---
+++ Desktop/iteach-master/Laravel5/resources/views/iteach/auth/login.blade.php
@@ -0,0 +1,50 @@
+@extends('iteach.auth.auth')
+
+@section('content')		
+						@if (count($data['errors']) > 0)
+						<div class="animatedParent">
+							<div class="animated bounceInDown">
+								<div class="col-md-9 xs-marginbot-20">
+									<strong>Whoops!</strong> There are problems in the input.<br>
+									<ul>
+										@foreach ($data['errors'] as $error)
+											{{ $error }}<br>
+										@endforeach
+									</ul>
+								</div>
+							</div>
+						</div>
+						@endif
+	
+						<form id="contact-form" role="form" method="GET" action="{{ url('/attempt_login') }}">
+						<input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+						<!-- Username & Password -->
+						<div class="row">
+							<div class="col-md-9 xs-marginbot-20">
+							<div class="form-group">
+								<input type="text" class="form-control input-lg" id="username" name="username" placeholder="Username" required="required" value="{{ old('username') }}"/>
+							</div>
+							</div>
+						</div>
+
+						<div class="row">
+							<div class="col-md-9">
+							<div class="form-group">
+								<input type="password" class="form-control input-lg" id="password" name="password" placeholder="Confirm Password" required="required" value="{{ old('password') }}"/>
+							</div>
+							</div>
+						</div>
+
+						<div class="row">
+							<div class="col-md-9">
+							<button type="submit" class="btn btn-skin btn-lg btn-block marginbot-10" id="btnContactUs">
+								Login
+							</button>
+							<a href="#" class="col-lg-offset-10">< Back</a>
+							</div>
+
+						</div>
+
+						</form>
+@endsection
