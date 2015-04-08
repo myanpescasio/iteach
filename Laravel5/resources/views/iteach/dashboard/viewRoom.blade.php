1532e31d8ad12cee51922da811372fb52a98c592
jopao06 <jopao06@gmail.com>
2015-04-09 04:11:07 +0800

    Established Connection between DB and Views (Except for ViewRoom)
    1. View All
       + Connected to DB
       + Needs Filtering of Data
    
    2. View Course
       + Connected to DB
       + Replaced Accordion with Dropdown
       + Needs to display details of course
    
    3. View Rooms
       + Not yet connected to DB
       + Added HTML code for making a schedule
       + Needs schedule to be dynamic
    
    4. View Instructor
       + Connected to DB
       + Can retrieve photos from system files (public/profile)
       + Added CSS for gallery
       + Needs to display details of instructor (Viewed via Modal)

--- Laravel5/resources/views/iteach/dashboard/viewRoom.blade.php
+++ Laravel5/resources/views/iteach/dashboard/viewRoom.blade.php
@@ -7,5 +7,151 @@
                             Rooms <small>Guest</small>
                         </h1>
                     </div>
-                </div>
+                    </div>
+                    <!--Reference: http://www.tablesgenerator.com/html_tables#-->
+                    <div class="bs-example">
+                    <div class="panel-group" id="accordion">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <h4 class="panel-title">
+                                    <a data-toggle="collapse" data-parent="#accordion1" href="#collapse1">ICS Conference Room</a>
+                                </h4>
+                            </div>
+                            <div id="collapse1" class="panel-collapse collapse">
+                                <div class="panel-body">
+                                	<table class="tg">
+									  <tr>
+									    <th class="tg-vx3v" colspan="6">ICS Conference Room</th>
+									  </tr>
+									  <tr>
+									    <td class="tg-7l82">Time / Day</td>
+									    <td class="tg-7l82">Monday</td>
+									    <td class="tg-7l82">Tuesday</td>
+									    <td class="tg-7l82">Wednesday</td>
+									    <td class="tg-7l82">Thursday</td>
+									    <td class="tg-7l82">Friday</td>
+									  </tr>
+									  <tr>
+									    <td class="tg-jecd">7:00 - 8:00</td>
+									    <td class="tg-031e"></td>
+									    <td class="tg-031e"></td>
+									    <td class="tg-031e"></td>
+									    <td class="tg-031e"></td>
+									    <td class="tg-031e"></td>
+									  </tr>
+									  <tr>
+									    <td class="tg-jecd">8:00 - 9:00</td>
+									    <td class="tg-031e"></td>
+									    <td class="tg-031e"></td>
+									    <td class="tg-031e"></td>
+									    <td class="tg-031e"></td>
+									    <td class="tg-031e"></td>
+									  </tr>
+									  <tr>
+									    <td class="tg-jecd">9:00 - 10:00</td>
+									    <td class="tg-031e"></td>
+									    <td class="tg-031e"></td>
+									    <td class="tg-031e"></td>
+									    <td class="tg-031e"></td>
+									    <td class="tg-031e"></td>
+									  </tr>
+									  <tr>
+									    <td class="tg-jecd">10:00 - 11:00</td>
+									    <td class="tg-031e"></td>
+									    <td class="tg-031e"></td>
+									    <td class="tg-031e"></td>
+									    <td class="tg-031e"></td>
+									    <td class="tg-031e"></td>
+									  </tr>
+									  <tr>
+									    <td class="tg-jecd">11:00 - 12:00</td>
+									    <td class="tg-031e"></td>
+									    <td class="tg-031e"></td>
+									    <td class="tg-031e"></td>
+									    <td class="tg-031e"></td>
+									    <td class="tg-031e"></td>
+									  </tr>
+									  <tr>
+									    <td class="tg-jecd">12:00 - 1:00</td>
+									    <td class="tg-031e"></td>
+									    <td class="tg-031e"></td>
+									    <td class="tg-031e"></td>
+									    <td class="tg-031e"></td>
+									    <td class="tg-031e"></td>
+									  </tr>
+									  <tr>
+									    <td class="tg-jecd">1:00 - 2:00</td>
+									    <td class="tg-031e"></td>
+									    <td class="tg-031e"></td>
+									    <td class="tg-031e"></td>
+									    <td class="tg-jyom" rowspan="3"><br>CMSC 341<br>WX<br>Pabico</td>
+									    <td class="tg-031e"></td>
+									  </tr>
+									  <tr>
+									    <td class="tg-jecd">2:00 - 3:00</td>
+									    <td class="tg-031e"></td>
+									    <td class="tg-031e"></td>
+									    <td class="tg-031e"></td>
+									    <td class="tg-031e"></td>
+									  </tr>
+									  <tr>
+									    <td class="tg-jecd">3:00 - 4:00</td>
+									    <td class="tg-031e"></td>
+									    <td class="tg-031e"></td>
+									    <td class="tg-031e"></td>
+									    <td class="tg-031e"></td>
+									  </tr>
+									  <tr>
+									    <td class="tg-jecd">4:00 - 5:00</td>
+									    <td class="tg-031e"></td>
+									    <td class="tg-nc2p" rowspan="3"><br>CMSC 271<br><br>YZ<br>KHAN</td>
+									    <td class="tg-jyom" rowspan="2">CMSC 180<br>GH <br>Pabico</td>
+									    <td class="tg-t3b7" rowspan="3"><br>CMSC 391<br>YZ<br>Mariano</td>
+									    <td class="tg-031e"></td>
+									  </tr>
+									  <tr>
+									    <td class="tg-jecd">5:00 - 6:00</td>
+									    <td class="tg-031e"></td>
+									    <td class="tg-031e"></td>
+									  </tr>
+									  <tr>
+									    <td class="tg-jecd">6:00 - 7:00</td>
+									    <td class="tg-031e"></td>
+									    <td class="tg-031e"></td>
+									    <td class="tg-031e"></td>
+									  </tr>
+									</table>
+                                </div>
+                            </div>
+                        </div>
+            			<div class="panel panel-default">
+                            <div class="panel-heading">
+                                <h4 class="panel-title">
+                                    <a data-toggle="collapse" data-parent="#accordion2" href="#collapse2">ICS GS Room</a>
+                                </h4>
+                            </div>
+                            <div id="collapse2" class="panel-collapse collapse">
+                                <div class="panel-body">
+                                    <p>Lorem ipsum dulum chuva chuchu. <a href="http://www.tutorialrepublic.com/html-tutorial/" target="_blank">Learn more.</a></p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <h4 class="panel-title">
+                                    <a data-toggle="collapse" data-parent="#accordion3" href="#collapse3">ICS LH 3</a>
+                                </h4>
+                            </div>
+                            <div id="collapse3" class="panel-collapse collapse">
+                                <div class="panel-body">
+                                    <p>Lorem ipsum dulum chuva chuchu.<a href="http://www.tutorialrepublic.com/html-tutorial/" target="_blank">Learn more.</a></p>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>        
+					
+
 @endsection
